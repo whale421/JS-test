087e0e1b2d725ed189f882127ac666d30bbe1de8
whale421 <62377374+whale421@users.noreply.github.com>
2020-03-29 23:48:30 +0800

    sort

--- test.js
+++ test.js
@@ -1,9 +1,33 @@
-let s = Symbol();
-
-let obj = {
-  [s]: function (arg) { 
-      return '123';
+var a=[3,44,38,5,47,15,36,26,27,2,46];
+function Sort(e) {
+   if(e.length <= 1) {
+      return e;}
+      else {
+      var middle = Math.floor(0.5*e.length);// 5
+      var left = e.slice(0,middle);// 3,44,38,5,47
+      var right = e.slice(middle,e.length);// 15,36,26,27,2,46
+      return Q(Sort(left),Sort(right));
    }
-};
-
-console.log(obj[s](123));
+}
+ function Q(L,R) {
+        var result = [];
+        var i = 0, j = 0;
+        while(i < L.length && j < R.length) {
+        if(L[i] > R[j]) {
+          result.push(R[j]);
+          j++;
+        }
+        else {
+          result.push(L[i]);
+          i++;
+        }
+      }
+      for(;i < L.length; i++) {
+         result.push(L[i]);
+       } 
+      for(;j < R.length; j++) {
+        result.push(R[j]);
+      } 
+      return result;
+    }
+    console.log(Sort(a)); 
